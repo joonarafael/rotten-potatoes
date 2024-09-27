977fa9899c995e88631a5ddfe7363ba1423cfbaa
Joona Kettunen <joona.kettunen21@gmail.com>
2024-09-27 09:35:50 +0300

    update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE genres (
 CREATE TABLE movies (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     title VARCHAR(255) NOT NULL UNIQUE,
-    description VARCHAR(1250) NOT NULL,
+    description VARCHAR(1024) NOT NULL,
     year INT NOT NULL,
     genre_id UUID REFERENCES genres(id) ON DELETE CASCADE NOT NULL,
     created_by UUID REFERENCES users(id),
@@ -28,15 +28,15 @@ CREATE TABLE movies (
 CREATE TABLE likes (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-    movie_id UUID REFERENCES movies(id) ON DELETE CASCADE NOT NULL,
-    rating INT NOT NULL
+    movie_id UUID REFERENCES movies(id) ON DELETE CASCADE NOT NULL
 );
 
 CREATE TABLE reviews (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id UUID REFERENCES users(id) ON DELETE CASCADE NOT NULL,
     movie_id UUID REFERENCES movies(id) ON DELETE CASCADE NOT NULL,
-    review VARCHAR(1250) NOT NULL,
+    rating INT NOT NULL,
+    comment VARCHAR(1250) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
