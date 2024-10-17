6aeb6c0ec1676032a2ba66db8c3c400f56f00db5
Joona Kettunen <joona.kettunen21@gmail.com>
2024-10-17 15:10:16 +0300

    ...and check style

--- src/app.py
+++ src/app.py
@@ -12,8 +12,8 @@ app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
 
-from routes import profile
-from routes import search
-from routes import index
-from routes import users
 from routes import movies
+from routes import users
+from routes import index
+from routes import search
+from routes import profile
